01a6027867cd7a9d3ea6a24dd5531967aab031dc
thejjwang <jonathanjacobwang@gmail.com>
2023-06-29 11:37:05 -0700

    added faded slideshow

--- index.js
+++ index.js
@@ -23,3 +23,14 @@ function randomQuote(arr) {
     return arr[index];
 }
 randomQuote(["I", "like", "javascript", "I", "love", "javascript"]);
+
+window.addEventListener("DOMContentLoaded", function(e) {
+
+    var stage = document.getElementById("stage");
+    var fadeComplete = function(e) { stage.appendChild(arr[0]); };
+    var arr = stage.getElementsByTagName("a");
+    for(var i=0; i < arr.length; i++) {
+      arr[i].addEventListener("animationend", fadeComplete, false);
+    }
+
+  }, false);
